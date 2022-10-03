2fd2e60838782b36df79a4d2c6c6ff0b853b70de
GTiko <107884976+GTiko@users.noreply.github.com>
2022-10-03 17:15:08 -0500

    up

--- Day2/q3.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const prompt = require("prompt-sync")();
-const age = prompt("How old are you ? ");
-function checkAge(age){
-    (age > 18 ) ?  console.log("true") : console.log("Did parents allow you?")
-}
-checkAge(age);
